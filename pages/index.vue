4704e0e1ed67fa3b5d1da3474ec1d474344d1aa6
陳建佑 <jianyou224@gmail.com>
2022-10-07 20:56:01 +0800

    環境建立

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+    <h2>test</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style lang="scss">
+  .box {
+    background: #fa0;
+    h2 {
+      text-align: center;
+      padding: 3rem 0;
+      color: #fff;
+    }
+  }
+</style>
